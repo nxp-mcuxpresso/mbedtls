7d4f5533b418011744c270e809934790131e978c
Lukas Zajac <lukas.zajac@nxp.com>
2022-01-27 22:30:46 +0100

    [SSROM-691] Merge of K4W1 A0 and A1 implemetation into one package.
    Signed-off-by: Lukas Zajac <lukas.zajac@nxp.com>

--- port/sssapi/sssapi_mbedtls.c
+++ port/sssapi/sssapi_mbedtls.c
@@ -1,3 +1,109 @@
+#if (defined(KW45_A0_SUPPORT) && KW45_A0_SUPPORT)
+/*
+ * Copyright 2019-2020 NXP
+ * All rights reserved.
+ *
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#if !defined(MBEDTLS_CONFIG_FILE)
+#include "mbedtls/config.h"
+#else
+#include MBEDTLS_CONFIG_FILE
+#endif
+
+#include "sssapi_mbedtls.h"
+#include "fsl_common.h"
+#include "fsl_snt.h"
+
+sss_sscp_key_store_t g_keyStore;
+sss_sscp_session_t g_sssSession;
+sscp_context_t g_sscpContext;
+static uint32_t g_isCryptoHWInitialized = SSS_CRYPTOHW_NONINITIALIZED;
+
+#define SSS_HIGH_QUALITY_RNG (0x1u)
+
+/******************************************************************************/
+/******************** CRYPTO_InitHardware **************************************/
+/******************************************************************************/
+/*!
+ * @brief Application init for various Crypto blocks.
+ *
+ * This function is provided to be called by MCUXpresso SDK applications.
+ * It calls basic init for Crypto Hw acceleration and Hw entropy modules.
+ */
+status_t CRYPTO_InitHardware(void)
+{
+    status_t ret = kStatus_Fail;
+    if (g_isCryptoHWInitialized == SSS_CRYPTOHW_NONINITIALIZED)
+    {
+        sss_sscp_rng_t rctx;
+        if (SNT_mu_wait_for_ready(ELEMUA, SSS_MAX_SUBSYTEM_WAIT) != kStatus_Success)
+        {
+        }
+#if (defined(SNT_HAS_LOADABLE_FW) && SNT_HAS_LOADABLE_FW)
+        else if (SNT_loadFwLocal(ELEMUA) != kStatus_SNT_Success)
+        {
+        }
+#endif /* SNT_HAS_LOADABLE_FW */
+        else if (sscp_mu_init(&g_sscpContext, (MU_Type *)(uintptr_t)ELEMUA) != kStatus_SSCP_Success)
+        {
+        }
+        else if (sss_sscp_open_session(&g_sssSession, SSS_SUBSYSTEM, &g_sscpContext, 0u, NULL) != kStatus_SSS_Success)
+        {
+        }
+        else if (sss_sscp_key_store_context_init(&g_keyStore, &g_sssSession) != kStatus_SSS_Success)
+        {
+        }
+        else if (sss_sscp_key_store_allocate(&g_keyStore, 0u) != kStatus_SSS_Success)
+        {
+        }
+        /* RNG call used to init Sentinel TRNG required e.g. by sss_sscp_key_store_generate_key service
+        if TRNG inicialization is no needed for used operations, the following code can be removed
+        to increase the performance.*/
+        else if (sss_sscp_rng_context_init(&g_sssSession, &rctx, SSS_HIGH_QUALITY_RNG) != kStatus_SSS_Success)
+        {
+        }
+        /*Providing NULL output buffer, as we just need to initialize TRNG, not get random data*/
+        else if (sss_sscp_rng_get_random(&rctx, NULL, 0x0u) != kStatus_SSS_Success)
+        {
+        }
+        else if (sss_sscp_rng_free(&rctx) != kStatus_SSS_Success)
+        {
+        }
+        else
+        {
+            g_isCryptoHWInitialized = SSS_CRYPTOHW_INITIALIZED;
+            ret                     = kStatus_Success;
+        }
+    }
+    else
+    {
+        if (g_isCryptoHWInitialized == SSS_CRYPTOHW_INITIALIZED)
+        {
+            ret = kStatus_Success;
+        }
+    }
+    return ret;
+}
+
+/*!
+ * @brief Application reinit for various Crypto blocks.
+ *
+ * This function is provided to be called after wake up from low power Power Down
+ * or Deep Power Down modes to reinit Crypto HW blocks.
+ */
+status_t CRYPTO_ReinitHardware(void)
+{
+    status_t ret;
+
+    g_isCryptoHWInitialized = SSS_CRYPTOHW_NONINITIALIZED;
+    ret                     = CRYPTO_InitHardware();
+
+    return ret;
+}
+#else
 /*
  * Copyright 2019-2020 NXP
  * All rights reserved.
@@ -57,7 +163,7 @@ status_t CRYPTO_InitHardware(void)
         }
         /* RNG call used to init Sentinel TRNG required e.g. by sss_sscp_key_store_generate_key service
         if TRNG inicialization is no needed for used operations, the following code can be removed
-        to increase the perfomance.*/
+        to increase the performance.*/
         else if (sss_sscp_rng_context_init(&g_sssSession, &rctx, SSS_HIGH_QUALITY_RNG) != kStatus_SSS_Success)
         {
         }
@@ -95,7 +201,8 @@ status_t CRYPTO_ReinitHardware(void)
     status_t ret;
 
     g_isCryptoHWInitialized = SSS_CRYPTOHW_NONINITIALIZED;
-    ret = CRYPTO_InitHardware();
+    ret                     = CRYPTO_InitHardware();
 
     return ret;
 }
+#endif /* KW45_A0_SUPPORT */
--- port/sssapi/sssapi_mbedtls.h
+++ port/sssapi/sssapi_mbedtls.h
@@ -1,3 +1,50 @@
+#if (defined(KW45_A0_SUPPORT) && KW45_A0_SUPPORT)
+/*
+ * Copyright 2019-2021 NXP
+ * All rights reserved.
+ *
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef SSSAPI_MBEDTLS_H
+#define SSSAPI_MBEDTLS_H
+
+#if defined(MBEDTLS_NXP_SENTINEL200)
+#define SSS_SUBSYSTEM (kType_SSS_Sentinel200)
+#elif defined(MBEDTLS_NXP_SENTINEL300)
+#define SSS_SUBSYSTEM (kType_SSS_Sentinel300)
+#else
+#define SSS_SUBSYSTEM (kType_SSS_Software)
+#endif
+
+#define SSS_MAX_SUBSYTEM_WAIT          (0xFFFFFFFFu)
+#define SSS_PUBLIC_KEY_PART_EXPORTABLE (0xF0u)
+#define SSS_FULL_KEY_EXPORTABLE        (0xFFu)
+#define SSS_CRYPTOHW_INITIALIZED       (0xF0F0F0F0u)
+#define SSS_CRYPTOHW_NONINITIALIZED    (0x0F0F0F0Fu)
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "fsl_sss_mgmt.h"
+#include "fsl_sss_sscp.h"
+#include "fsl_sscp_mu.h"
+
+extern sss_sscp_key_store_t g_keyStore;
+extern sss_sscp_session_t g_sssSession;
+extern sscp_context_t g_sscpContext;
+
+status_t CRYPTO_InitHardware(void);
+status_t CRYPTO_ReinitHardware(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SSSAPI_MBEDTLS_H */
+#else
 /*
  * Copyright 2019-2021 NXP
  * All rights reserved.
@@ -49,3 +96,4 @@ status_t CRYPTO_ReinitHardware(void);
 #endif
 
 #endif /* SSSAPI_MBEDTLS_H */
+#endif /* KW45_A0_SUPPORT */

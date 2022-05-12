c5752570583ffbdccc57de5ddbe764401cc328c7
Vit Chmelar (nxf49003) <vit.chmelar@nxp.com>
2022-05-12 13:40:54 +0200

    [MCUX-50189] Added NXP comment about modifying buffer length in mbedtls original release

--- library/ccm.c
+++ library/ccm.c
@@ -471,6 +471,8 @@ int mbedtls_ccm_auth_decrypt( mbedtls_ccm_context *ctx, size_t length,
  */
 
 #define NB_TESTS 3
+
+/* NXP increased CCM_SELFTEST_PT_MAX_LEN from 24 to 32 for HW accelerators support */ 
 #define CCM_SELFTEST_PT_MAX_LEN 32
 #define CCM_SELFTEST_CT_MAX_LEN 32
 

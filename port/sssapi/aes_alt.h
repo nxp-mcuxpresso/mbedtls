8de32d63d01fe2fa7a78fb9660d56642fc80a15e
Vit Chmelar <vit.chmelar@nxp.com>
2022-08-11 14:08:55 +0200

    [MCUX-35393] Remove SW function which is not used in SSSAPI AES port

--- port/sssapi/aes_alt.h
+++ port/sssapi/aes_alt.h
@@ -58,26 +58,6 @@ typedef struct mbedtls_aes_context
 
 } mbedtls_aes_context;
 
-#else
-// Regular implementation
-//
-
-/**
- * \brief The AES context-type definition.
- */
-typedef struct
-{
-    int nr;           /*!< The number of rounds. */
-    uint32_t *rk;     /*!< AES round keys. */
-    uint32_t buf[68]; /*!< Unaligned data buffer. This buffer can
-                           hold 32 extra Bytes, which can be used for
-                           one of the following purposes:
-                           <ul><li>Alignment if VIA padlock is
-                                   used.</li>
-                           <li>Simplifying key expansion in the 256-bit
-                               case by generating an extra round key.
-                               </li></ul> */
-} mbedtls_aes_context;
 #endif /* MBEDTLS_AES_ALT */
 
 #ifdef __cplusplus

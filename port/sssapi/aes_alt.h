60e95689fedd869dd00b20ee6b64c305dfad5068
Vit Chmelar <vit.chmelar@nxp.com>
2022-08-19 11:20:57 +0200

    [MCUX-35393] Added support for AES-XTS in SSSAPI port, reformatted aes_alt.c to according aes.c to have same structure (eq. this function is after this function) to make future updates easier

--- port/sssapi/aes_alt.h
+++ port/sssapi/aes_alt.h
@@ -58,6 +58,19 @@ typedef struct mbedtls_aes_context
 
 } mbedtls_aes_context;
 
+#if defined(MBEDTLS_CIPHER_MODE_XTS)
+/**
+ * \brief The AES XTS context-type definition.
+ */
+typedef struct mbedtls_aes_xts_context
+{
+    mbedtls_aes_context crypt; /*!< The AES context to use for AES block
+                                        encryption or decryption. */
+    mbedtls_aes_context tweak; /*!< The AES context used for tweak
+                                        computation. */
+} mbedtls_aes_xts_context;
+#endif /* MBEDTLS_CIPHER_MODE_XTS */
+
 #endif /* MBEDTLS_AES_ALT */
 
 #ifdef __cplusplus

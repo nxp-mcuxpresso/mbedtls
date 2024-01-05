3fde8090b08dfa30da436e56c6b43f55aa17696a
Anke Xiao <anke.xiao@nxp.com>
2024-01-05 15:37:35 +0800

    [MCUX-65956][K4W1][K32W1] MbedtLS example has no output
    - Defined the macro MBEDTLS_PLATFORM_PRINTF_MACRO to specify the output
      to debug console.
    
    Signed-off-by: Anke Xiao <anke.xiao@nxp.com>

--- port/sssapi/sssapi_mbedtls_config.h
+++ port/sssapi/sssapi_mbedtls_config.h
@@ -3986,7 +3986,7 @@
 //#define MBEDTLS_PLATFORM_TIME_MACRO            time /**< Default time macro to use, can be undefined. MBEDTLS_HAVE_TIME must be enabled */
 //#define MBEDTLS_PLATFORM_TIME_TYPE_MACRO       time_t /**< Default time macro to use, can be undefined. MBEDTLS_HAVE_TIME must be enabled */
 //#define MBEDTLS_PLATFORM_FPRINTF_MACRO      fprintf /**< Default fprintf macro to use, can be undefined */
-//#define MBEDTLS_PLATFORM_PRINTF_MACRO        PRINTF /**< Default printf macro to use, can be undefined */
+#define MBEDTLS_PLATFORM_PRINTF_MACRO        PRINTF /**< Default printf macro to use, can be undefined */
 /* Note: your snprintf must correctly zero-terminate the buffer! */
 //#define MBEDTLS_PLATFORM_SNPRINTF_MACRO    snprintf /**< Default snprintf macro to use, can be undefined */
 //#define MBEDTLS_PLATFORM_VSNPRINTF_MACRO    vsnprintf /**< Default vsnprintf macro to use, can be undefined */

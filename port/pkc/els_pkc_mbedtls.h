9b0c81c48cea03d125d83980763bfdeb83a7fdc2
Mcu_sdk Ci <mcu_sdk.ci@nxp.com>
2024-05-16 07:39:38 +0200

    Apply MCUXpresso SDK 2.14.0 changes.

---
+++ port/pkc/els_pkc_mbedtls.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2022 NXP
+ * All rights reserved.
+ *
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef ELS_PKC_MBEDTLS_H
+#define ELS_PKC_MBEDTLS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#if defined(MBEDTLS_THREADING_C)
+/* Threading mutex implementations for mbedTLS. */
+#include "mbedtls/threading.h"
+#include "threading_alt.h"
+#endif
+
+
+#define ELS_PKC_CRYPTOHW_INITIALIZED    (0xF0F0F0F0U)
+#define ELS_PKC_CRYPTOHW_NONINITIALIZED (0x0F0F0F0FU)
+
+int fsl_mbedtls_printf(const char *fmt_s, ...);
+status_t CRYPTO_InitHardware(void);
+
+#if defined(MBEDTLS_THREADING_C)
+/* MUTEX FOR HW Modules*/
+extern mbedtls_threading_mutex_t mbedtls_threading_hwcrypto_els_mutex;
+extern mbedtls_threading_mutex_t mbedtls_threading_hwcrypto_pkc_mutex;
+#endif /* defined(MBEDTLS_THREADING_C) */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ELS_PKC_MBEDTLS_H */
--- set_middleware_mbedtls.cmake
+++ set_middleware_mbedtls.cmake
@@ -41,29 +41,83 @@ endif()
 endif()
 
 
-if (CONFIG_USE_middleware_mbedtls_port_ksdk)
-# Add set(CONFIG_USE_middleware_mbedtls_port_ksdk true) in config.cmake to use this component
+if (CONFIG_USE_middleware_mbedtls_port_els_pkc)
+# Add set(CONFIG_USE_middleware_mbedtls_port_els_pkc true) in config.cmake to use this component
 
-message("middleware_mbedtls_port_ksdk component is included from ${CMAKE_CURRENT_LIST_FILE}.")
+message("middleware_mbedtls_port_els_pkc component is included from ${CMAKE_CURRENT_LIST_FILE}.")
 
-if(CONFIG_USE_middleware_mbedtls AND CONFIG_USE_middleware_mbedtls_template AND ((CONFIG_USE_driver_sha AND CONFIG_USE_driver_rng AND CONFIG_USE_driver_aes AND (CONFIG_DEVICE_ID STREQUAL LPC54S005 OR CONFIG_DEVICE_ID STREQUAL LPC54S016 OR CONFIG_DEVICE_ID STREQUAL LPC54S018 OR CONFIG_DEVICE_ID STREQUAL LPC54S018J2M OR CONFIG_DEVICE_ID STREQUAL LPC54S018J4M)) OR (CONFIG_USE_driver_sha AND CONFIG_USE_driver_rng AND (CONFIG_DEVICE_ID STREQUAL LPC54005 OR CONFIG_DEVICE_ID STREQUAL LPC54016 OR CONFIG_DEVICE_ID STREQUAL LPC54018 OR CONFIG_DEVICE_ID STREQUAL LPC54018J2M OR CONFIG_DEVICE_ID STREQUAL LPC54018J4M OR CONFIG_DEVICE_ID STREQUAL LPC54628J512)) OR (CONFIG_USE_driver_rng AND (CONFIG_DEVICE_ID STREQUAL LPC54605J512 OR CONFIG_DEVICE_ID STREQUAL LPC54605J256 OR CONFIG_DEVICE_ID STREQUAL LPC54606J512 OR CONFIG_DEVICE_ID STREQUAL LPC54606J256 OR CONFIG_DEVICE_ID STREQUAL LPC54607J256 OR CONFIG_DEVICE_ID STREQUAL LPC54607J512 OR CONFIG_DEVICE_ID STREQUAL LPC54608J512 OR CONFIG_DEVICE_ID STREQUAL LPC54616J512 OR CONFIG_DEVICE_ID STREQUAL LPC54616J256 OR CONFIG_DEVICE_ID STREQUAL LPC54618J512)) OR (CONFIG_USE_driver_rng_1 AND CONFIG_USE_driver_casper AND CONFIG_USE_driver_hashcrypt AND (CONFIG_DEVICE_ID STREQUAL LPC55S04 OR CONFIG_DEVICE_ID STREQUAL LPC55S06 OR CONFIG_DEVICE_ID STREQUAL LPC55S14 OR CONFIG_DEVICE_ID STREQUAL LPC55S16 OR CONFIG_DEVICE_ID STREQUAL LPC55S26 OR CONFIG_DEVICE_ID STREQUAL LPC55S28 OR CONFIG_DEVICE_ID STREQUAL LPC55S66 OR CONFIG_DEVICE_ID STREQUAL LPC55S69)) OR (CONFIG_USE_driver_trng AND CONFIG_USE_driver_casper AND CONFIG_USE_driver_hashcrypt AND (CONFIG_DEVICE_ID STREQUAL MIMXRT533S OR CONFIG_DEVICE_ID STREQUAL MIMXRT555S OR CONFIG_DEVICE_ID STREQUAL MIMXRT595S OR CONFIG_DEVICE_ID STREQUAL MIMXRT633S OR CONFIG_DEVICE_ID STREQUAL MIMXRT685S)) OR (CONFIG_USE_driver_dcp AND CONFIG_USE_driver_trng AND CONFIG_USE_driver_cache_armv7_m7 AND (CONFIG_DEVICE_ID STREQUAL MIMXRT1011xxxxx OR CONFIG_DEVICE_ID STREQUAL MIMXRT1021xxxxx OR CONFIG_DEVICE_ID STREQUAL MIMXRT1024xxxxx OR CONFIG_DEVICE_ID STREQUAL MIMXRT1041xxxxB OR CONFIG_DEVICE_ID STREQUAL MIMXRT1042xxxxB OR CONFIG_DEVICE_ID STREQUAL MIMXRT1051xxxxB OR CONFIG_DEVICE_ID STREQUAL MIMXRT1052xxxxB OR CONFIG_DEVICE_ID STREQUAL MIMXRT1061xxxxA OR CONFIG_DEVICE_ID STREQUAL MIMXRT1061xxxxB OR CONFIG_DEVICE_ID STREQUAL MIMXRT1062xxxxA OR CONFIG_DEVICE_ID STREQUAL MIMXRT1062xxxxB OR CONFIG_DEVICE_ID STREQUAL MIMXRT1064xxxxA)) OR (CONFIG_USE_driver_trng AND CONFIG_USE_middleware_mmcau_cm0p AND (CONFIG_DEVICE_ID STREQUAL K32L2A31xxxxA OR CONFIG_DEVICE_ID STREQUAL K32L2A41xxxxA)) OR (CONFIG_USE_driver_trng AND CONFIG_USE_middleware_mmcau_cm4_cm7 AND (CONFIG_DEVICE_ID STREQUAL K32L2A41xxxxA)) OR (CONFIG_USE_driver_rnga AND CONFIG_USE_middleware_mmcau_cm4_cm7 AND (CONFIG_DEVICE_ID STREQUAL MKM35Z512xxx7 OR CONFIG_DEVICE_ID STREQUAL MKM35Z256xxx7)) OR (CONFIG_USE_driver_rnga AND CONFIG_USE_middleware_mmcau_cm0p AND (CONFIG_DEVICE_ID STREQUAL MKM35Z512xxx7 OR CONFIG_DEVICE_ID STREQUAL MKM35Z256xxx7)) OR (CONFIG_USE_driver_cau3 AND CONFIG_USE_driver_trng AND (CONFIG_DEVICE_ID STREQUAL K32L3A60xxx)) OR (CONFIG_USE_driver_trng AND CONFIG_USE_driver_ltc AND CONFIG_USE_middleware_mmcau_cm4_cm7 AND (CONFIG_DEVICE_ID STREQUAL MCIMX7U5xxxxx)) OR (CONFIG_USE_driver_caam AND (CONFIG_DEVICE_ID STREQUAL MIMXRT1165xxxxx OR CONFIG_DEVICE_ID STREQUAL MIMXRT1166xxxxx OR CONFIG_DEVICE_ID STREQUAL MIMXRT1171xxxxx OR CONFIG_DEVICE_ID STREQUAL MIMXRT1172xxxxx OR CONFIG_DEVICE_ID STREQUAL MIMXRT1173xxxxx OR CONFIG_DEVICE_ID STREQUAL MIMXRT1175xxxxx OR CONFIG_DEVICE_ID STREQUAL MIMXRT1176xxxxx))))
+if(CONFIG_USE_middleware_mbedtls_port_els AND CONFIG_USE_component_els_pkc AND CONFIG_USE_middleware_mbedtls_els_pkc_config AND (((CONFIG_DEVICE_ID STREQUAL MCXN235 OR CONFIG_DEVICE_ID STREQUAL MCXN236))))
 
 target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
-  ${CMAKE_CURRENT_LIST_DIR}/./port/ksdk/ksdk_mbedtls.c
-  ${CMAKE_CURRENT_LIST_DIR}/./port/ksdk/des_alt.c
-  ${CMAKE_CURRENT_LIST_DIR}/./port/ksdk/aes_alt.c
-  ${CMAKE_CURRENT_LIST_DIR}/./port/ksdk/ecp_alt.c
-  ${CMAKE_CURRENT_LIST_DIR}/./port/ksdk/ecp_curves_alt.c
-  ${CMAKE_CURRENT_LIST_DIR}/./port/ksdk/ecp_alt_ksdk.c
+  ${CMAKE_CURRENT_LIST_DIR}/./port/pkc/ecc_alt.c
+  ${CMAKE_CURRENT_LIST_DIR}/./port/pkc/ecdh_alt.c
+  ${CMAKE_CURRENT_LIST_DIR}/./port/pkc/ecdsa_alt.c
+  ${CMAKE_CURRENT_LIST_DIR}/./port/pkc/rsa_alt.c
+  ${CMAKE_CURRENT_LIST_DIR}/./port/pkc/els_pkc_mbedtls.c
 )
 
 target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
-  ${CMAKE_CURRENT_LIST_DIR}/./port/ksdk
+  ${CMAKE_CURRENT_LIST_DIR}/./port/pkc
 )
+
+if(CONFIG_USE_COMPONENT_CONFIGURATION)
+  message("===>Import configuration from ${CMAKE_CURRENT_LIST_FILE}")
+
+  target_compile_definitions(${MCUX_SDK_PROJECT_NAME} PUBLIC
+    -DMBEDTLS_MCUX_ELS_PKC_API
+    -DMBEDTLS_MCUX_USE_PKC
+    -DMBEDTLS_CONFIG_FILE="els_pkc_mbedtls_config.h"
+  )
+
+endif()
 
 else()
 
-message(SEND_ERROR "middleware_mbedtls_port_ksdk dependency does not meet, please check ${CMAKE_CURRENT_LIST_FILE}.")
+message(SEND_ERROR "middleware_mbedtls_port_els_pkc dependency does not meet, please check ${CMAKE_CURRENT_LIST_FILE}.")
+
+endif()
+
+endif()
+
+
+if (CONFIG_USE_middleware_mbedtls_port_els)
+# Add set(CONFIG_USE_middleware_mbedtls_port_els true) in config.cmake to use this component
+
+message("middleware_mbedtls_port_els component is included from ${CMAKE_CURRENT_LIST_FILE}.")
+
+if(CONFIG_USE_component_els_pkc_els AND CONFIG_USE_middleware_mbedtls)
+
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+  ${CMAKE_CURRENT_LIST_DIR}/./port/els/aes_alt.c
+  ${CMAKE_CURRENT_LIST_DIR}/./port/els/cbc_mac_alt.c
+  ${CMAKE_CURRENT_LIST_DIR}/./port/els/cmac_alt.c
+  ${CMAKE_CURRENT_LIST_DIR}/./port/els/ctr_drbg_alt.c
+  ${CMAKE_CURRENT_LIST_DIR}/./port/els/gcm_alt.c
+  ${CMAKE_CURRENT_LIST_DIR}/./port/els/sha256_alt.c
+  ${CMAKE_CURRENT_LIST_DIR}/./port/els/sha512_alt.c
+  ${CMAKE_CURRENT_LIST_DIR}/./port/els/entropy_poll_alt.c
+  ${CMAKE_CURRENT_LIST_DIR}/./port/els/els_mbedtls.c
+)
+
+target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
+  ${CMAKE_CURRENT_LIST_DIR}/./port/els
+)
+
+if(CONFIG_USE_COMPONENT_CONFIGURATION)
+  message("===>Import configuration from ${CMAKE_CURRENT_LIST_FILE}")
+
+  target_compile_definitions(${MCUX_SDK_PROJECT_NAME} PUBLIC
+    -DMBEDTLS_MCUX_ELS_API
+    -DMBEDTLS_MCUX_USE_ELS
+    -DMCUXCL_FEATURE_CSSL_MEMORY_C_FALLBACK
+    -DMBEDTLS_CONFIG_FILE="els_mbedtls_config.h"
+  )
+
+endif()
+
+else()
+
+message(SEND_ERROR "middleware_mbedtls_port_els dependency does not meet, please check ${CMAKE_CURRENT_LIST_FILE}.")
 
 endif()
 
@@ -75,7 +129,7 @@ if (CONFIG_USE_middleware_mbedtls)
 
 message("middleware_mbedtls component is included from ${CMAKE_CURRENT_LIST_FILE}.")
 
-if(CONFIG_USE_utility_debug_console AND (CONFIG_USE_middleware_mbedtls_port_ksdk))
+if(CONFIG_USE_utility_debug_console AND (CONFIG_USE_middleware_mbedtls_port_els_pkc))
 
 target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
   ${CMAKE_CURRENT_LIST_DIR}/./library/aes.c
@@ -187,6 +241,12 @@ if(CONFIG_USE_COMPONENT_CONFIGURATION)
   if(CONFIG_TOOLCHAIN STREQUAL iar)
     target_compile_options(${MCUX_SDK_PROJECT_NAME} PUBLIC
       --diag_suppress Pa167,Pe177,Pe191,Pe546
+    )
+  endif()
+  if(CONFIG_TOOLCHAIN STREQUAL armgcc)
+    target_compile_options(${MCUX_SDK_PROJECT_NAME} PUBLIC
+      -fomit-frame-pointer
+      -Wno-unused-function
     )
   endif()
 
